2afef9be88e35bfc17d8c077abdf42d76571834f
praveenbabu36 <praveen.babu36@gmail.com>
2025-05-30 00:11:12 -0400

    Understand Class Name and File Name

---
+++ J21-Code/basics/src/main/java/com/praveen/ClassName_Ex_1.java
@@ -0,0 +1,14 @@
+package com.praveen;
+
+// public class name should match with file name
+public class ClassName_Ex_1{
+    public static void main(String[] args) {
+        System.out.println("In Class 1");
+    }
+}
+
+ class ClassName_Ex_2 {
+    public static void main(String[] args) {
+        System.out.println("In Class 2");
+    }
+}
---
+++ J21-Code/basics/src/main/java/com/praveen/ClassName_Ex_2.java
@@ -0,0 +1,28 @@
+package com.praveen;
+
+// A java file can have any number of non-public classes, it doesn't need to match with file name.
+// if there is a public class, its name should match with file name.
+
+class ClassName_Ex_3 {
+    public static void main(String[] args) {
+        System.out.println("In Class 3");
+    }
+}
+
+class ClassName_Ex_4 {
+    public static void main(String[] args) {
+        System.out.println("In Class 3");
+    }
+}
+
+class ClassName_Ex_5 {
+    public static void main(String[] args) {
+        System.out.println("In Class 3");
+    }
+}
+
+class ClassName_Ex_6 {
+    public static void main(String[] args) {
+        System.out.println("In Class 3");
+    }
+}
