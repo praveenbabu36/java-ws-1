59fcf8da04c919fdb351be3da8587ffcaedb2e1c
praveenbabu36 <praveen.babu36@gmail.com>
2025-05-31 09:32:21 -0400

    Exception and Comment Examples

---
+++ J21-Code/basics/src/main/java/com/praveen/Comment_Ex_4.java
@@ -0,0 +1,35 @@
+package com.praveen;
+
+public class Comment_Ex_4 {
+
+    public static void main(String[] args) {
+
+        // Single Line Comment
+        // int a = 'a';
+
+        /*
+            Multi Line Comment
+         */
+        /*
+            int i = getId("TEST");
+            System.out.println(i);
+         */
+
+        System.out.println(" Test \"Hello\" World ");
+
+    }
+
+
+    /**
+     * Comment for Documentation
+     * Explain Method Usage / Signature / Return Types
+     * @param name
+     * @return int
+     */
+    public int getId(String name){
+        return -1;
+    }
+
+}
+
+
---
+++ J21-Code/basics/src/main/java/com/praveen/Exception_Ex_3.java
@@ -0,0 +1,19 @@
+package com.praveen;
+
+public class Exception_Ex_3 {
+
+    public static void main(String[] args) {
+
+        // Compile time error - in the below step to see the compilation error.
+        // remove the ';' to see the error
+        int i = -1;
+
+        // Runtime error - ArithmeticException - Divide by zero.
+        // uncomment to see the error
+        int b = 100/0;
+
+
+
+
+    }
+}
