3a4362326a0df3a563342cac3da5a28dbb255b50
praveenbabu36 <praveen.babu36@gmail.com>
2025-05-31 09:51:43 -0400

    Variable Declaration and Initialization Ex

---
+++ J21-Code/basics/src/main/java/com/praveen/Variables_Ex_5.java
@@ -0,0 +1,30 @@
+package com.praveen;
+
+public class Variables_Ex_5 {
+
+
+    public static void main(String[] args) {
+
+        // Declaration
+        int var1;
+
+        // Initialization
+        var1 = 100;
+
+        System.out.println("Value of var1:" + var1);
+
+
+        // Declaration and Initialization Together
+        int var2 = 200;
+
+        System.out.println("Value of var2:" + var2);
+
+
+        // Change the value of var - Reassign
+        var2 = 400;
+
+        System.out.println("Value of var2:" + var2);
+
+    }
+
+}
