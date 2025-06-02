be0e918ecab645602f31e8c3d14b5bb04ac91f4f
praveenbabu36 <praveen.babu36@gmail.com>
2025-06-01 23:43:03 -0400

    Boolean and Char Example

---
+++ J21-Code/basics/src/main/java/com/praveen/Boolean_Char_Ex_9.java
@@ -0,0 +1,26 @@
+package com.praveen;
+
+public class Boolean_Char_Ex_9 {
+
+    public static void main(String[] args) {
+
+        // boolean
+        boolean isHoliday = true;
+
+        System.out.println(isHoliday);
+
+        // char
+        char c1 = 'z';
+
+        System.out.println(c1);
+
+
+        // Store Unicode in Char
+        char c2 = '\u0121';
+
+        System.out.println(c2);
+
+
+    }
+
+}
